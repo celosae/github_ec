9796b7729dceeb1146bde5a9fbb011b399e405cf
César López <clopezs@iescomercio.com>
2024-03-19 19:48:44 +0100

    Iniciar Repositorio

---
+++ HolaMundo/src/Principal.java
@@ -0,0 +1,17 @@
+//import java.util.Scanner;
+
+public class Principal {
+	
+	public static void main (String args[]) {
+		int n = 5;
+		System.out.println("Hola Mundo\n");
+		 do {
+			 n--;
+			 System.out.println(n);}
+			 while (n != -1);
+
+		}
+	
+		
+
+}
