be8f61dc778165fad6d960973483844b54c04263
César López <clopezs@iescomercio.com>
2024-03-20 11:05:01 +0100

    Modificar Javadoc para ejemplo

--- HolaMundo/src/Principal.java
+++ HolaMundo/src/Principal.java
@@ -1,7 +1,17 @@
+// TODO: Auto-generated Javadoc
 //import java.util.Scanner;
 
+/**
+ * The Class Principal.
+ * Hola mundo para ejemplo.
+ */
 public class Principal {
 	
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main (String args[]) {
 		int n = 5;
 		System.out.println("Hola Mundo\n");
